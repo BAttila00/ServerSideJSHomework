65e88bff94457cb2ffae84933ea1d0fb495e569b
BAttila <attila.bruder@business-solution.hu>
2020-10-30 21:28:09 +0100

    turazok.ejs és turazokModList.ejs már a middleware-ből kapják az adatot. res.locals.turazok-on keresztül

--- middleware/turazo/getTopTurazokMW.js
+++ middleware/turazo/getTopTurazokMW.js
@@ -4,6 +4,22 @@ const requireOption = require('../requireOption');
 
 module.exports = function (objectrepository) {
     return function (req, res, next) {
+        res.locals.turazok = [
+            {
+                _id: '001',
+                nev: 'Attila',
+                kor: 22,
+                nem: 'Férfi',
+                email: 'asdghjrtzoppő@gmial.com'
+            },
+            {
+                _id: '002',
+                nev: 'Attila II',
+                kor: 25,
+                nem: 'Férfi',
+                email: 'kléűáuipő@gmial.com'
+            }
+        ];
         next();
     };
 };
