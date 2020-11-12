9f48cdc7e8f2b6cee2f9981f1d2377551539eaa7
BAttila00 <bruderattila@gmail.com>
2020-11-12 17:30:01 +0100

    getTurakMW - egy adott turázó túráinak kikeresése valamint megjelenítése;

--- middleware/tura/getTurakMW.js
+++ middleware/tura/getTurakMW.js
@@ -3,7 +3,19 @@
 const requireOption = require('../requireOption');
 
 module.exports = function (objectrepository) {
+
+    const TuraModel = requireOption(objectrepository, 'TuraModel');
+
     return function (req, res, next) {
-        next();
+
+        TuraModel.find({_turazo: res.locals.turazo._id}, (err, turak) => {
+            if (err) {
+                return next(err);
+            }
+
+            res.locals.turak = turak;
+            return next();
+        });
+
     };
 };
